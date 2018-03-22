7408ab6f5413c5cb0956722a8368d9b94d2c5d23
Victor Borja <vborja@apache.org>
2018-03-22 03:44:35 -0600

    Add Maybe test using Union Patterns.

---
+++ test/expat_maybe_test.exs
@@ -0,0 +1,36 @@
+defmodule Expat.MaybeTest do
+  use ExUnit.Case
+  use Expat
+
+  @moduledoc ~S"""
+  Using Unions to implement a simple Maybe type.
+
+  Any non-nil value is an instance of Just.
+
+  See also: expat_union_test.exs
+  """
+
+  defpat maybe(v)
+  | nothing(nil)
+  | ( just(y) when not is_nil(y) )
+
+  test "nothing is nil" do
+    assert nil == nothing!()
+  end
+
+  test "just is non nil" do
+    assert 22 = just!(22)
+  end
+
+  test "just can be pattern matched" do
+    assert just() = :jordan
+  end
+
+  test "nil cannot be pattern matched with just" do
+    expat case nil do
+      just() -> raise "Should not happen"
+      nothing() -> assert :ok
+    end
+  end
+
+end
